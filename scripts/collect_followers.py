6f12ba5629744b7a975b91aabba4afb56d755a0b
take-cmd-web <nakashimatakenobu@gmail.com>
2026-05-08 20:33:41 +0900

    Create collect_followers.py

---
+++ scripts/collect_followers.py
@@ -0,0 +1,19 @@
+import os, csv, datetime
+import tweepy
+
+client = tweepy.Client(bearer_token=os.environ["X_BEARER_TOKEN"])
+me = client.get_me(user_fields=["public_metrics"])
+m = me.data.public_metrics
+
+today = datetime.date.today().isoformat()
+os.makedirs("data", exist_ok=True)
+path = "data/followers.csv"
+new_file = not os.path.exists(path)
+
+with open(path, "a", newline="") as f:
+    w = csv.writer(f)
+    if new_file:
+        w.writerow(["date", "followers", "following", "tweets"])
+    w.writerow([today, m["followers_count"], m["following_count"], m["tweet_count"]])
+
+print(f"Recorded: {today} followers={m['followers_count']}")
